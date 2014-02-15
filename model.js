a671c8b1afdb24aecbd082e038ed9d29324a8635
megalord <jwgriege@sbcglobal.net>
2014-02-14 18:01:56 -0600

    Updated model to use dot notation for getting and setting properties.

--- model.js
+++ model.js
@@ -1,60 +1,53 @@
-// The model is dependent on diesel.Events
-
 var diesel = (function(parent) {
 
 parent.Model = {
 
     // Create a new object using the Model prototype.
-    create:function() {
+    create:function(data) {
         var self = Object.create(this);
-        self._data = {};
         parent.Events.extendTo(self);
+        self.defineMany(data);
         return self;
     },
 
-    // Define a property of the model
-    // using a static value of generator function.
     define:function(property, value) {
-        var dependencies, fn, fnStr, match, pattern;
-        // If a function is used to define the property,
-        // it is provided in the 'value' argument.
+        var fnStr, match,
+            dependencies = Array.prototype.slice.call(arguments, 2),
+            pattern = /this\.([\w\.]+)/g,
+            self = this;
+
+        Object.defineProperty(this, property, {
+            configurable:false,
+            enumerable:true,
+            get:typeof value === 'function' ? value.bind(self) : function() { return value; },
+            set:function(newValue) {
+                value = newValue;
+                self.trigger('changed:' + property, self[property], property);
+            }
+        });
+
         if(typeof value === 'function') {
-            fn = value;
-            value = fn.call(this);
-            // Extract the dependencies from all the 'get' calls inside the function.
-            dependencies = [];
-            fnStr = fn.toString();
-            pattern = /this\.get\('(\w+)'\)/g;
+            // Extract the dependencies from all the references to 'this' inside the function.
+            fnStr = value.toString();
             while(match = pattern.exec(fnStr))
                 if(dependencies.indexOf(match[1]) === -1)
                     dependencies.push(match[1]);
-            // Listen for changes in the dependencies and re-run the generator function
-            // so that the property's value is always up to date.
+
+            // Listen for changes in the dependencies to propogate events.
             for(var i = 0, ilen = dependencies.length; i < ilen; i++) {
-                this.on('changed:' + dependencies[i], function(value) {
-                    this.set(property, fn.call(this));
+                this.on('changed:' + dependencies[i], function() {
+                    this.trigger('changed:' + property, this[property], property);
                 });
             };
         };
-        this._data[property] = value;
-        // Return the model instance for chaining.
+
         return this;
     },
 
-    // Retrieve a property of the model.
-    get:function(property) {
-        return this._data[property];
-    },
-
-    // Change the value of a property of the model.
-    // Trigger a change event that may propogate to other properties.
-    set:function(property, value) {
-        if(property in this._data) {
-            this._data[property] = value;
-            this.trigger('changed:' + property, value, property);
-        };
+    defineMany:function(data) {
+        for(var prop in data)
+            this.define(prop, data[prop]);
     }
-
 };
 
 return parent;
