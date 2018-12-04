97130839e79a7fa28ffdeb3f25d57dcbb652d3ab
megalord <jordan.griege@rackspace.com>
2018-12-03 19:07:43 -0600

    indentation

--- events.js
+++ events.js
@@ -1,65 +1,65 @@
 var diesel = (function(parent) {
 
-parent.Events = {
+  parent.Events = {
 
     _registry:{},
 
     extendTo:function(target) {
-        for(var prop in this)
-            target[prop] = this[prop];
+      for(var prop in this)
+        target[prop] = this[prop];
     },
 
     // Add a callback function to the registry for an event.
     // This function will be called when the event is fired.
     on:function(event, callback, context) {
-        if(Array.isArray(event)) {
-            for(var i = 0; i < event.length; i++)
-                this.on(event[i], callback, context);
-            return;
-        };
+      if(Array.isArray(event)) {
+        for(var i = 0; i < event.length; i++)
+          this.on(event[i], callback, context);
+        return;
+      };
 
-        if(!(event in this._registry))
-            this._registry[event] = [];
-        this._registry[event].push({
-            callback:callback,
-            context:context || this
-        });
+      if(!(event in this._registry))
+        this._registry[event] = [];
+      this._registry[event].push({
+        callback:callback,
+        context:context || this
+      });
     },
 
     // This is the same idea as on(),
     // but the callback will be removed from the registry after it is run.
     once:function(event, callback, context) {
-        var self = this,
+      var self = this,
         fn = function() {
-            callback.apply(this, arguments);
-            self.off(event, fn);
+          callback.apply(this, arguments);
+          self.off(event, fn);
         };
-        this.on(event, fn, context);
+      this.on(event, fn, context);
     },
 
     // Remove a callback function to the registry for an event.
     off:function(event, callback) {
-        if(event in this._registry) {
-            for(var i = 0, ilen = this._registry[event].length; i < ilen; i++) {
-                if(this._registry[event][i].callback === callback) {
-                    this._registry[event].splice(i, 1);
-                    break;
-                };
-            };
+      if(event in this._registry) {
+        for(var i = 0, ilen = this._registry[event].length; i < ilen; i++) {
+          if(this._registry[event][i].callback === callback) {
+            this._registry[event].splice(i, 1);
+            break;
+          };
         };
+      };
     },
 
     // Trigger an event, firing all registered callbacks.
     trigger:function(/* event [, args] */) {
-        var args = Array.prototype.slice.call(arguments),
-            event = args.shift();
-        if(event in this._registry)
-            for(var i = 0, ilen = this._registry[event].length; i < ilen; i++)
-                this._registry[event][i].callback.apply(this._registry[event][i].context, args);
+      var args = Array.prototype.slice.call(arguments),
+        event = args.shift();
+      if(event in this._registry)
+        for(var i = 0, ilen = this._registry[event].length; i < ilen; i++)
+          this._registry[event][i].callback.apply(this._registry[event][i].context, args);
     }
 
-};
+  };
 
-return parent;
+  return parent;
 
 })(diesel || {});
--- model.js
+++ model.js
@@ -1,75 +1,75 @@
 var diesel = (function(parent) {
 
-parent.Model = {
+  parent.Model = {
 
     // Create a new object using the Model prototype.
     create:function(data) {
-        var self = Object.create(this);
+      var self = Object.create(this);
 
-        // Extend the model with the events.
-        if(!('Events' in parent))
-            throw new Error('Model requires diesel.Events.');
-        parent.Events.extendTo(self);
+      // Extend the model with the events.
+      if(!('Events' in parent))
+        throw new Error('Model requires diesel.Events.');
+      parent.Events.extendTo(self);
 
-        // Define the data on the new model if any has been passed.
-        if(typeof data === 'object' && Object.keys(data).length > 0)
-            self.define(data);
+      // Define the data on the new model if any has been passed.
+      if(typeof data === 'object' && Object.keys(data).length > 0)
+        self.define(data);
 
-        return self;
+      return self;
     },
 
     define:function(property, value) {
-        var fn, fnStr, match,
-            dependencies = Array.prototype.slice.call(arguments, 2),
-            pattern = /this\.([\w\.]+)/g,
-            self = this;
+      var fn, fnStr, match,
+        dependencies = Array.prototype.slice.call(arguments, 2),
+        pattern = /this\.([\w\.]+)/g,
+        self = this;
 
-        // The property argument is optionally an object containing multiple properties.
-        if(typeof property === 'object') {
-            for(var prop in property)
-                this.define(prop, property[prop]);
-            return;
-        };
+      // The property argument is optionally an object containing multiple properties.
+      if(typeof property === 'object') {
+        for(var prop in property)
+          this.define(prop, property[prop]);
+        return;
+      };
 
-        if(typeof value === 'function') {
-            fn = value;
+      if(typeof value === 'function') {
+        fn = value;
+        value = fn.call(this);
+      };
+
+      Object.defineProperty(this, property, {
+        configurable:false,
+        enumerable:true,
+        get:function() { return value; },
+        set:function(newValue) {
+          if(newValue !== value) {
+            value = newValue;
+            self.trigger('changed:' + property, self[property], property);
+          };
+        }
+      });
+
+      // Add event listeners to the dependencies so that changes are propagated.
+      if(typeof fn === 'function') {
+        // Extract the dependencies from all the references to 'this' inside the function.
+        fnStr = fn.toString();
+        while(match = pattern.exec(fnStr))
+          if(dependencies.indexOf(match[1]) === -1)
+            dependencies.push(match[1]);
+
+        // Listen for changes in the dependencies to propagate events.
+        for(var i = 0, ilen = dependencies.length; i < ilen; i++) {
+          this.on('changed:' + dependencies[i], function() {
             value = fn.call(this);
+            this.trigger('changed:' + property, this[property], property);
+          });
         };
+      };
 
-        Object.defineProperty(this, property, {
-            configurable:false,
-            enumerable:true,
-            get:function() { return value; },
-            set:function(newValue) {
-                if(newValue !== value) {
-                    value = newValue;
-                    self.trigger('changed:' + property, self[property], property);
-                };
-            }
-        });
-
-        // Add event listeners to the dependencies so that changes are propagated.
-        if(typeof fn === 'function') {
-            // Extract the dependencies from all the references to 'this' inside the function.
-            fnStr = fn.toString();
-            while(match = pattern.exec(fnStr))
-                if(dependencies.indexOf(match[1]) === -1)
-                    dependencies.push(match[1]);
-
-            // Listen for changes in the dependencies to propagate events.
-            for(var i = 0, ilen = dependencies.length; i < ilen; i++) {
-                this.on('changed:' + dependencies[i], function() {
-                    value = fn.call(this);
-                    this.trigger('changed:' + property, this[property], property);
-                });
-            };
-        };
-
-        return this;
+      return this;
     }
 
-};
+  };
 
-return parent;
+  return parent;
 
 })(diesel || {});
