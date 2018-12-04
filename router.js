d602cdf874e2ddcae63b8486d6a395b9acd21ed3
megalord <jordan.griege@rackspace.com>
2018-12-03 19:11:47 -0600

    update router

--- router.js
+++ router.js
@@ -1,151 +1,128 @@
-var diesel = (function(parent) {
+(function(parent) {
 
-  parent.router = {
+  let routes = {};
 
-    _started:false,
+  let notFoundFn = function (path) {
+    throw new Error(`No route matches ${path}`);
+  }
 
-    _routes:{},
-
-    _settings:{
-      history:true,
-      onError:function(route) {
-        throw new Error('Route does not exist.', route);
-      }
-    },
-
-    // Executes the function(s) for a route
-    // and all of it's parent routes, if any are found.
-    _parse:function(route) {
-      var base = '',
-        found = false,
-        isValid = false,
-        matchingRoutes = Object.keys(this._routes),
-        param = '',
-        params = {},
-        partial = '/',
-        parts = route.split('/').slice(1),
-        prefix = '';
-
-      // Provide a general hook for all routes.
-      if('*' in this._routes) this._routes['*'](route);
-
-      // Sequentially build the full route from its parts,
-      // checking for sub-route callbacks at each step
-      // and populating the params object if necessary.
-      for(var i = 0, ilen = parts.length; i < ilen; i++) {
-        // Update the partial route string.
-        base = partial + prefix;
-        partial = base + parts[i];
-
-        // Reflect the changes to the partial route in the array of filtered routes.
-        matchingRoutes = matchingRoutes.filter(function(route) {
-          return route.indexOf(base) === 0;
-        });
-
-        if(partial in this._routes) {
-          this._routes[partial](params);
-          isValid = true;
-        } else {
-          // Search for a route with a parameter,
-          // which can be identified by a ':' character after the '/'.
-          found = false;
-          param = '';
-          for(var j = 0, jlen = matchingRoutes.length; j < jlen; j++) {
-            if(matchingRoutes[j].charAt(base.length) === ':') {
-              found = true;
-              break;
-            };
-          };
-          if(found) {
-            param = matchingRoutes[j].slice(base.length + 1).split('/')[0];
-            params[param] = parts[i];
-
-            // Fix the partial route string.
-            partial = base + ':' + param;
-            if(partial in this._routes) {
-              this._routes[partial](params);
-              isValid = true;
-            } else {
-              isValid = false;
-            };
-          } else {
-            isValid = false;
-          };
-        };
-
-        // The prefix for the next part of the route
-        // should be a '/' character for every iteration but the first.
-        // e.g. sub1 /sub2 /page   <- spaces added for demonstration
-        prefix = '/';
-      };
-      if(!isValid) this._settings.onError(route);
-    },
-
-    _routeChangeHandler:function() {
-      var route = location[this._settings.history ? 'pathname' : 'hash'];
-
-      // Remove everything up to and including the first '/' character.
-      route = route.slice(route.indexOf('/'));
-
-      // A fix for the index page when the url hash is used but not set.
-      if(route === '') route = '/';
-
-      this._parse(route);
-    },
-
-    // Configure the router's settings.
-    // This should be called before start() for expected operation.
-    config:function(newSettings) {
-      for(var prop in newSettings) {
-        this._settings[prop] = newSettings[prop];
-      };
-      return this;
-    },
-
-    // Programatically navigate to a route.
-    navigateTo:function(route) {
-      if(!this._started) {
-        throw new Error('Attempted to navigate before starting router.');
-        return false;
-      };
-
-      if(this._settings.history) {
-        history.pushState(null, '', route);
-        this._routeChangeHandler();
-      } else {
-        location.hash = route;
-      };
-    },
-
-    // Start monitoring history or hash changes.
-    start:function() {
-      this._started = true;
-
-      // Ensure that the history api is available if it is used.
-      if(this._settings.history && !('history' in window))
-        this._settings.history = false;
-
-      // Attach the handler to the change event
-      // and call it to get the resources for the initial route.
-      window.onpopstate = function() {
-        diesel.router._routeChangeHandler();
-      };
-      this._routeChangeHandler();
-    },
-
-    stop:function() {
-      this._started = false;
-      window.onpopstate = function() {};
-    },
-
-    // Assign a callback function to a route.
-    when:function(route, callback) {
-      this._routes[route] = callback;
-      return this;
+  function matches(pathParts, routeParts) {
+    if (pathParts.length !== routeParts.length) {
+      return null;
     }
 
+    let params = {};
+    for (let i = 0; i < routeParts.length; i++) {
+      if (routeParts[i][0] === ':') {
+        params[routeParts[i].slice(1)] = pathParts[i];
+      } else if (pathParts[i] !== routeParts[i]) {
+        return null;
+      }
+    }
+
+    return params;
+  }
+
+  function parse(path) {
+    let params;
+    let pathParts = path.split('/');
+
+    for (let route in routes) {
+      if (route === 'else') {
+        continue;
+      }
+
+      params = matches(pathParts, route.split('/'));
+      if (params) {
+        return routes[route].bind(null, params);
+      }
+    }
+    return notFoundFn.bind(null, path);
+  }
+
+  function routeChangeHandler() {
+    let path = location.pathname;
+
+    if (path === '') {
+      path = '/';
+    }
+
+    parse(path)();
+  }
+
+  function router(_routes) {
+    routes = _routes;
+
+    if ('else' in routes) {
+      notFoundFn = routes.else;
+    }
+
+    document.addEventListener('click', function(event) {
+      if (event.target.tagName != 'A' || event.ctrlKey || event.metaKey || ('button' in event && event.button != 0)) {
+        return;
+      }
+
+      let href = event.target.getAttribute('href');
+      if (!href || href.includes('http')) {
+        return;
+      }
+
+      event.preventDefault();
+      router.go(href);
+    });
+
+    window.onpopstate = routeChangeHandler;
+    // Get the resources for the initial route.
+    routeChangeHandler();
+  }
+
+  // Programatically navigate to a route.
+  router.go = function(route, options) {
+    options || (options = {});
+    history[options.replace ? 'replaceState' : 'pushState'](null, '', route);
+
+    // Adding a state entry does not trigger the `popstate` window event.
+    routeChangeHandler();
   };
 
-  return parent;
+  parent.router = router;
+
+})(diesel || {});
+
+(function(parent) {
+
+  let recurse = (prefix, routes, pres) => {
+    if (routes.pre) {
+      pres = pres.concat([routes.pre]);
+    }
+
+    let ret = {};
+    for (let route in routes) {
+      if (route === 'pre') {
+        continue;
+      }
+
+      let path = route === 'self' ? prefix : (prefix + route);
+      if (routes[route].constructor === Function) {
+        //ret[path] = (params) => console.log(path, params, pres.concat([routes[route]]));
+        ret[path] = (params) => pres.concat([routes[route]]).every((fn) => fn(params) !== false);
+      } else if (route === 'self') {
+        throw new TypeError(`groupedRoutes for self on ${path} must be a Function, got ${routes.constructor}`);
+      } else if (routes[route].constructor === Object) {
+        Object.assign(ret, recurse(path, routes[route], pres));
+      } else {
+        throw new TypeError(`groupedRoutes for path ${path} expected Function or Object, got ${routes[route].constructor}`);
+      }
+    }
+    return ret;
+  };
+
+  parent.groupedRoutes = (routes) => {
+    if (routes.constructor !== Object) {
+      throw new TypeError(`groupedRoutes expected Object, got ${routes.constructor}`);
+    }
+
+    return recurse('', routes, []);
+  };
 
 })(diesel || {});
